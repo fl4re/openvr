4eb8b62a40921318e9e01029e421a5e7c0656e3c
Vadim Sloockiy <2trustnik@gmail.com>
2017-05-23 14:31:34 +0200

    prades to package.json

--- publish_binary_on_mac.sh
+++ publish_binary_on_mac.sh
@@ -2,10 +2,13 @@
 
 # make sure we are inside a git checkout
 git rev-parse --is-inside-work-tree
+
+# install deps
+npm install
+
 # npm version patch w/o git commit & git tag
 npm --no-git-tag-version version patch
-# install npm prades
-npm install @sb/prades
+
 # finally prades publish
 node_modules/.bin/prades publish -v
 
